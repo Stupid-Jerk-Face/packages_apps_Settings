LiquidRemix/android_packages_apps_Settings

settings: activity to launch headsup settings
Change-Id: I6587a1bf7e132e10759b5a9d858ddfd3ed866b22

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -133,6 +133,7 @@ import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.liquid.liquidlounge.LiquidLoungeSettings;
+import com.liquid.liquidlounge.fragments.HeadsUpSettings;
 
 public class SettingsGateway {
 
@@ -256,6 +257,7 @@ public class SettingsGateway {
             BluetoothDeviceDetailsFragment.class.getName(),
             DataUsageList.class.getName(),
 			LiquidLoungeSettings.class.getName(),
+            HeadsUpSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

Stupid-Jerk-Face/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -131,8 +131,8 @@ import com.android.settings.wifi.WifiAPITest;
 import com.android.settings.wifi.WifiInfo;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
-
 import com.six.settings.SixSettings;
+import com.six.settings.fragments.HeadsUpSettings;
 
 public class SettingsGateway {
 
@@ -256,6 +256,7 @@ public class SettingsGateway {
             BluetoothDeviceDetailsFragment.class.getName(),
             DataUsageList.class.getName(),
 			SixSettings.class.getName(),
+            HeadsUpSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {